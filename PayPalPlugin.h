aaea4dab8865ab0ee11363a7cc8dc8506a6b6549
SnareChops <snare_chops@yahoo.com>
2013-06-08 15:03:03 -0400

    Update PayPalPlugin.h

--- PayPalPlugin.h
+++ PayPalPlugin.h
@@ -1,11 +1,12 @@
-//
-//  PayPalPlugin.h
-//  iOS Pay Pal Plugin
-//
-//  Created by Scott Robinson on 6/5/13.
-//
-//  Note: DO NOT MODIFY THIS FILE
-
+/*****************************************
+ *  PayPalPlugin.h
+ *
+ *  Created by Scott Robinson on 6/5/13.
+ *
+ *  Version 2.0b
+ *****************************************
+ * Implementation Note: DO NOT MODIFY THIS FILE
+ *****************************************/
 #import <Foundation/Foundation.h>
 #import <Cordova/CDVPlugin.h>
 #import "PayPalMobile.h"
